8e11c234634b19b02a5b63c42115b73835d022dd
Basav Bamrah <80160941+basavbamrah@users.noreply.github.com>
2021-04-05 00:10:09 +0530

    Update StrongLanguage.cpp

--- StrongLanguage.cpp
+++ StrongLanguage.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 	int n,k,t;
+        cout<< "Enter a number:\n";
 	cin>>t;
 	for(int i=0;i<t;i++)
 	{
+            cout<<" Enter the length of string and least number of consecutive *:\n";
 	    cin>> n;
 	    cin>>k;
 	    string s;
+            cout<<"Enter the String:\n";
 	    cin>>s;
 	    int c=0,f=0;
 	    for(int j=0;j<s.length();j++)
